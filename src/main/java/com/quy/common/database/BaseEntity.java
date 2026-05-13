bc0168229a319f1717a7cf2cf79252aa42f37cff
DangQuy <dangquyy1108@gmail.com>
2026-05-13 22:52:38 +0700

    refactor: change to ZonedDateTime

--- src/main/java/com/quy/common/database/BaseEntity.java
+++ src/main/java/com/quy/common/database/BaseEntity.java
@@ -5,18 +5,16 @@ package com.quy.common.database;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import java.time.ZonedDateTime;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.io.Serializable;
-import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -30,7 +28,7 @@ public abstract class BaseEntity implements Serializable {
 
     @CreationTimestamp
     @Column(name = "created_at", updatable = false)
-    private LocalDateTime createdAt;
+    private ZonedDateTime createdAt;
 
     @LastModifiedBy
     @Column(name = "updated_by")
@@ -38,5 +36,5 @@ public abstract class BaseEntity implements Serializable {
 
     @UpdateTimestamp
     @Column(name = "updated_at")
-    private LocalDateTime updatedAt;
+    private ZonedDateTime updatedAt;
 }
